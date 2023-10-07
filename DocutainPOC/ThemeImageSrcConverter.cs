cd9a581267161ad71924bbfc3612f1ec010c00f6
Umar Rehman <omarrehman@gmail.com>
2023-10-07 15:51:53 +0500

    Initial commit

---
+++ DocutainPOC/ThemeImageSrcConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace DocutainPOC
+{
+    public class ThemeImageSrcConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string ImgSrc = (string)value;
+
+            AppTheme oSAppTheme = Application.Current.RequestedTheme;
+            if (oSAppTheme == AppTheme.Light)
+            {
+                return ImgSrc + "black.png";
+            }
+            else
+            {
+                return ImgSrc + "white.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return AppTheme.Dark;
+        }
+    }
+}
